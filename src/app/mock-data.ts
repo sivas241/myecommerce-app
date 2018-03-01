679d945759370f157a501777bfd95f7fd93921b1
rashmiraokr <rashmirao.kr@gmail.com>
2018-03-01 20:39:50 +0530

    first commit

---
+++ src/app/mock-data.ts
@@ -0,0 +1,323 @@
+export const DATA: any  = {
+  'categories': [
+    {
+      'categori_id': 1,
+      'name': 'Television'
+    },
+    {
+      'categori_id': 2,
+      'name': 'Refrigerators'
+    },
+    {
+      'categori_id': 3,
+      'name': 'Fashion'
+    },
+    {
+      'categori_id': 4,
+      'name': 'Mobile'
+    }
+  ],
+  'products': [
+    {
+      'id': 1,
+      'name': 'Brand Name...',
+      'price': '60.000',
+      'available': true,
+      'best_seller': true,
+      'categories': [
+        1
+      ],
+      'img': './assets/images/tv1.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 2,
+      'name': 'Brand Name...',
+      'price': '20.000',
+      'available': false,
+      'best_seller': false,
+      'categories': [
+        1
+      ],
+      'img': './assets/images/tv2.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 3,
+      'name': 'Brand Name...',
+      'price': '10.000',
+      'available': true,
+      'best_seller': true,
+      'categories': [
+        1
+      ],
+      'img': './assets/images/tv3.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 4,
+      'name': 'Brand Name...',
+      'price': '35.000',
+      'available': false,
+      'best_seller': false,
+      'categories': [
+        1
+      ],
+      'img': './assets/images/tv4.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 5,
+      'name': 'Brand Name...',
+      'price': '12.000',
+      'available': true,
+      'best_seller': true,
+      'categories': [
+        1
+      ],
+      'img': './assets/images/tv5.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 6,
+      'name': 'Brand Name...',
+      'price': '120.000',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        1
+      ],
+      'img': './assets/images/tv6.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 7,
+      'name': 'Brand Name...',
+      'price': '50.000',
+      'available': false,
+      'best_seller': false,
+      'categories': [
+        1
+      ],
+      'img': './assets/images/tv7.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 8,
+      'name': 'Brand Name...',
+      'price': '2000',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        3
+      ],
+      'img': './assets/images/fas1.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 9,
+      'name': 'Brand Name...',
+      'price': '2200',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        3
+      ],
+      'img': './assets/images/fas2.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 10,
+      'name': 'Brand Name...',
+      'price': '2700',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        
+        3
+      ],
+      'img': './assets/images/fas3.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 11,
+      'name': 'Brand Name...',
+      'price': '3000',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        3
+      ],
+      'img': './assets/images/fas4.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 12,
+      'name': 'Brand Name...',
+      'price': '4000',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+       3
+      ],
+      'img': './assets/images/fas5.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 13,
+      'name': 'Brand Name...',
+      'price': '2000',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+      3
+      ],
+      'img': './assets/images/fas6.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 14,
+      'name': 'Brand Name...',
+      'price': '2900',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+       3
+      ],
+      'img': './assets/images/fas7.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 15,
+      'name': 'Brand Name...',
+      'price': '7000',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+       3
+      ],
+      'img': './assets/images/fas8.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 16,
+      'name': 'Brand Name...',
+      'price': '150.000',
+      'available': true,
+      'best_seller': true,
+      'categories': [
+        2
+      ],
+      'img': './assets/images/ref1.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 17,
+      'name': 'Brand Name...',
+      'price': '200.000',
+      'available': false,
+      'best_seller': true,
+      'categories': [
+        2
+      ],
+      'img': './assets/images/ref2.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 17,
+      'name': 'Brand Name...',
+      'price': '200.000',
+      'available': false,
+      'best_seller': true,
+      'categories': [
+        2
+      ],
+      'img': './assets/images/ref2.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 18,
+      'name': 'Brand Name...',
+      'price': '200.000',
+      'available': false,
+      'best_seller': true,
+      'categories': [
+        4
+      ],
+      'img': './assets/images/mob1.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 19,
+      'name': 'Brand Name...',
+      'price': '100.000',
+      'available': false,
+      'best_seller': true,
+      'categories': [
+        4
+      ],
+      'img': './assets/images/mob2.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 20,
+      'name': 'Brand Name...',
+      'price': '250.000',
+      'available': false,
+      'best_seller': true,
+      'categories': [
+        4
+      ],
+      'img': './assets/images/mob3.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 21,
+      'name': 'Brand Name...',
+      'price': '250.000',
+      'available': false,
+      'best_seller': true,
+      'categories': [
+        4
+      ],
+      'img': './assets/images/mob4.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 22,
+      'name': 'Brand Name...',
+      'price': '290.000',
+      'available': false,
+      'best_seller': true,
+      'categories': [
+        4
+      ],
+      'img': './assets/images/mob5.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 23,
+      'name': 'Brand Name...',
+      'price': '300.000',
+      'available': false,
+      'best_seller': true,
+      'categories': [
+        4
+      ],
+      'img': './assets/images/mob6.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      'id': 24,
+      'name': 'Brand Name...',
+      'price': '280.000',
+      'available': false,
+      'best_seller': true,
+      'categories': [
+        4
+      ],
+      'img': './assets/images/mob7.jpg',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    }
+  ]
+};
